2b653e1cfa497c3f84d3ba15db5936d61b7f6713
Mohamed Hamada <mohamed.hamada@stud.h-da.de>
2026-08-27 00:41:05 +0200

    Delete Termin2 directory

--- Termin2/src/Beispiele.java
+++
@@ -1,9 +0,0 @@
-
-public class Beispiele {
-    public static final String SEQ20 = "10100110100101100101";
-    public static final String SEQ24 = "110010010010010010010011";
-    public static final String SEQ25 = "0010011000011000011000011";
-    public static final String SEQ36 = "000110011000001111111001100001100100";
-    public static final String SEQ48 = "001001100110000011111111110000001100110010011111";
-    public static final String SEQ50 = "11010101011110100010001000010001000101111010101011";
-}
